a867f0d82ef7fdafd718acfaf450681f7a5688e4
Timothy <lemayiant4@gmail.com>
2022-08-26 09:31:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+function findMatching(array,neno) {
+    return array.filter(hakuna => hakuna.toLowerCase().includes(neno.toLowerCase())).sort();
+}
+
+function fuzzyMatch(array, word) {
+    return array.filter(firstLetter => firstLetter.startsWith(word));
+}
+
+function matchName (array,namesy) {
+    return array.filter(jina => jina.name ===namesy);
+}
